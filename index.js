987b7304c2dc1b66ead4be27627737f461c660dc
Siddhartha <siddharthac@iitbhilai.ac.in>
2020-11-05 17:07:43 +0530

    Javascript Update 1 KeyDown & Click listeners, Sound Added

---
+++ index.js
@@ -0,0 +1,57 @@
+/*Code for keyboard inputs*/
+document.addEventListener("keydown", function (event) {
+	console.log(event);
+	makeSound(event.key);
+});
+
+/*Code for clicks*/
+var drumButtons = document.querySelectorAll(".drum");
+console.log(drumButtons.length);
+for (var i = 0; i < drumButtons.length; i++) {
+	drumButtons[i].addEventListener("click", function (event) {
+		makeSound(this.innerHTML);
+	});
+}
+
+/*Code to generate sound on click/keydown event*/
+function makeSound(key) {
+	switch (key) {
+		case "w":
+			var sound = new Audio("sounds/tom-1.mp3");
+			sound.play();
+			break;
+
+		case "a":
+			var sound = new Audio("sounds/tom-2.mp3");
+			sound.play();
+			break;
+
+		case "s":
+			var sound = new Audio("sounds/tom-3.mp3");
+			sound.play();
+			break;
+
+		case "d":
+			var sound = new Audio("sounds/tom-4.mp3");
+			sound.play();
+			break;
+
+		case "j":
+			var sound = new Audio("sounds/snare.mp3");
+			sound.play();
+			break;
+
+		case "k":
+			var sound = new Audio("sounds/kick-bass.mp3");
+			sound.play();
+			break;
+
+		case "l":
+			var sound = new Audio("sounds/crash.mp3");
+			sound.play();
+			break;
+
+		default:
+			console.log("Key not configured");
+	}
+}
